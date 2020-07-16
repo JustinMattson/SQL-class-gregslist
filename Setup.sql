2abd07b4ab1bba99c0c809ce8f25ec9f07cfb74e
Justin Mattson <justin.mattson@outlook.com>
2020-07-16 16:15:13 -0600

    appears all is functional

--- Setup.sql
+++ Setup.sql
@@ -1,32 +1,32 @@
-/* CREATE TABLE cars(
- id INT NOT NULL AUTO_INCREMENT,
- make VARCHAR(255) NOT NULL,
- model VARCHAR(255) NOT NULL,
- imgUrl VARCHAR(255) NOT NULL,
- body VARCHAR(255) NOT NULL,
- price INT NOT NULL,
- productionYear INT NOT NULL,
- userId VARCHAR(255) NOT NULL,
- PRIMARY KEY (id)
-) */
+USE gregslist252;
+-- CREATE TABLE cars(
+--  id INT NOT NULL AUTO_INCREMENT,
+--  make VARCHAR(255) NOT NULL,
+--  model VARCHAR(255) NOT NULL,
+--  imgUrl VARCHAR(255) NOT NULL,
+--  body VARCHAR(255) NOT NULL,
+--  price INT NOT NULL,
+--  productionYear INT NOT NULL,
+--  userId VARCHAR(255) NOT NULL,
+--  PRIMARY KEY (id)
+-- );
 
 
--- ALTER TABLE cars CHANGE productionYear year INT NOT NULL
+-- ALTER TABLE cars CHANGE productionYear year INT NOT NULL;
 
 
-USE gregslist6;
-CREATE TABLE carfavorites
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    carId INT NOT NULL,
-    user VARCHAR(255) NOT NULL,
+-- CREATE TABLE carfavorites
+-- (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     carId INT NOT NULL,
+--     user VARCHAR(255) NOT NULL,
 
-    PRIMARY KEY (id),
+--     PRIMARY KEY (id),
 
-    FOREIGN KEY (carId)
-        REFERENCES cars (id)
-        ON DELETE CASCADE
-)
+--     FOREIGN KEY (carId)
+--         REFERENCES cars (id)
+--         ON DELETE CASCADE
+-- );
 
 
 
